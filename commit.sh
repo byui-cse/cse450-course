175f4589f9580580798ccf0efb778810cd538dc0
Kyle Tolliver <43971202+kctolli@users.noreply.github.com>
2020-09-19 16:11:02 -0600

    commit.sh TBD

---
+++ commit.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+cd D:/CSE450
+git add --all
+timestamp() {
+  date +"at %H:%M on %m/%d/%Y"
+}
+git commit -am "Update $(timestamp)"
+git pull
+git push origin master
+
+# Log the commit
+
+repo="Machine Learning"
+
+## Declare file
+cd C:/RunFiles
+log=commit_log.txt
+ 
+## append commit to log file
+printf "Commit ${repo} $(timestamp)" >> $log
